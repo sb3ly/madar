d543bdf3e6527f86b3117bdd78fce950bc09501a
Saeed <90979595+sb3ly@users.noreply.github.com>
2025-06-15 14:49:23 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import './index.css'; // استيراد ملف الـCSS العام
+import './index.css'; // هاذا الملف اللي تسخت فيه كل اكواد ال css ياعطوه
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
